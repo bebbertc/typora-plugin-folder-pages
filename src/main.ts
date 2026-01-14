2742fcf4f79944b2ce12fe1416b8dedeb50f6933
Sergei Zaimovskii <zaims@mail.broward.edu>
2026-01-14 10:11:38 +0300

    Отключил NotionTreeMode, не нужен

--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,34 @@ import { Plugin } from "@typora-community-plugin/core";
 import { FolderNode } from "./FolderNode";
 import { nodeIsFolder, NotionTreeMode } from "./NotionTreeMode";
 
-function getFolderNodeFromTarget(target: HTMLElement): HTMLElement | null {
-  if (!target) return null;
-
-  // Проверяем, что клик был внутри панели библиотеки файлов
-  if (!target.closest("#file-library-tree")) return null;
-
-  // Ищем ближайший узел файла или папки
-  const node = target.closest(".file-library-node") as HTMLElement | null;
-  if (!node) return null;
-
-  // Проверяем, что это папка
-  if (!nodeIsFolder(node)) return null;
-
-  // Исключаем стрелку раскрытия/сворачивания
-  if (target.closest(".file-node-open-state")) return null;
-
-  return node;
-}
-
 export default class FolderNotesDebug extends Plugin {
+  private NOTION_TREE_MODE_ON = false;
+
+  getFolderNodeFromTarget(target: HTMLElement): HTMLElement | null {
+    if (!target) return null;
+
+    // Проверяем, что клик был внутри панели библиотеки файлов
+    if (!target.closest("#file-library-tree")) return null;
+
+    // Ищем ближайший узел файла или папки
+    const node = target.closest(".file-library-node") as HTMLElement | null;
+    if (!node) return null;
+
+    // Проверяем, что это папка
+    if (!nodeIsFolder(node)) return null;
+
+    // Исключаем стрелку раскрытия/сворачивания
+    if (target.closest(".file-node-open-state")) return null;
+
+    return node;
+  }
+
   private onClick = (e: MouseEvent) => {
     // Игнорируем не пользовательские события
     if (!e.isTrusted) return;
 
     const target = e.target as HTMLElement;
-    const folderNode = getFolderNodeFromTarget(target);
+    const folderNode = this.getFolderNodeFromTarget(target);
     if (!folderNode) return;
 
     // ⚠️ ВАЖНО: мы забираем клик себе, Typora не должна делать toggle сама, никакие другие события не должны сработать
@@ -44,8 +46,11 @@ export default class FolderNotesDebug extends Plugin {
   onload() {
     // capture оставляем, но теперь мы гасим событие
     document.addEventListener("click", this.onClick, true);
-    const notionTreeMode = new NotionTreeMode();
-    notionTreeMode.start();
+
+    if (this.NOTION_TREE_MODE_ON) {
+      const notionTreeMode = new NotionTreeMode();
+      notionTreeMode.start();
+    }
   }
 
   onunload() {
